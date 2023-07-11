ddf33f3ee5ee494e923fef5e6db39b95c2fdbbd2
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-11 23:03:37 +0530

    Check Year is Leap year or Not Leap Year

---
+++ LeapYear.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionalProgrammingProblems
+{
+    class LeapYear
+    {
+        public bool FindLeapYear(int year)
+        {
+            bool isLeapYear = false;
+            if (year % 4 == 0)
+            {
+                if (year % 100 != 0 || year % 400 == 0)                
+                    isLeapYear = true;                
+            }
+            return isLeapYear;
+        }
+    }
+    
+}
--- Program.cs
+++ Program.cs
@@ -6,8 +6,24 @@ namespace FunctionalProgrammingProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Functional Programming Problems");
-            FlipCoin.FlipCoinAndCalculatePercentage();
+            try
+            {
+                Console.WriteLine("Welcome to Functional Programming Problems");
+                var leapYear = new LeapYear();
+                Console.WriteLine("Enter a year (4-digit):");
+                int year = Convert.ToInt32(Console.ReadLine());
+                bool isLeapYear = leapYear.FindLeapYear(year);
+
+                if (isLeapYear)
+                    Console.WriteLine(year + " is a leap year.");
+                else
+                    Console.WriteLine(year + " is not a leap year.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Please Enter only Number (4-Digit) Year");
+            }
+
         }
     }
 }
