89c43b6bb0ec2131a63eefeabf41a11f678e65fc
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-11 22:47:02 +0530

    Flip coins and check head or tail and calculate percentage

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace FunctionalProgrammingProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Functional Programming Problems");
+            FlipCoin.FlipCoinAndCalculatePercentage();
         }
     }
 }
