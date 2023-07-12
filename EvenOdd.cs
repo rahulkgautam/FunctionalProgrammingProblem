5cfd9da61bb08654a05882e0bfe78d54473dd3b8
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-12 22:35:12 +0530

    Check Input number is Even or Odd

---
+++ EvenOdd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionalProgrammingProblems
+{
+    class EvenOdd
+    {
+        public void CheckEvenOddNumber(int number)
+        {
+            if (number % 2 == 0)
+                Console.WriteLine(number + " is a Even Number");
+            else
+                Console.WriteLine(number+" is a Odd Number");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,18 +6,19 @@ namespace FunctionalProgrammingProblems
     {
         static void Main(string[] args)
         {
-            // read in one command-line argument
-            int n = Convert.ToInt32(args[0]);
-            int i = 0;                // count from 0 to N
-            int powerOfTwo = 1;       // the ith power of two
-
-            // repeat until i equals n
-            while (i <= n)
+            try
             {
-               Console.WriteLine(i + " " + powerOfTwo);   // print out the power of two
-                powerOfTwo = 2 * powerOfTwo;                // double to get the next one
-                i = i + 1;
+                Console.WriteLine("=========Even odd number Problem===========");
+                var evenOdd = new EvenOdd();
+                Console.WriteLine("Enter a Number");
+                int number = Convert.ToInt32(Console.ReadLine());
+                evenOdd.CheckEvenOddNumber(number);
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please Enter only Number");
+            }
+            
         }
     }
 }
